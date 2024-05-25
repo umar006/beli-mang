d7f8dd31e679fadb97d986ca3778ca1f41a351ce
umar006 <umaralfaruq597@gmail.com>
2024-05-25 21:27:23 +0700

    feat: register admin api

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,14 +1,28 @@
 package server
 
 import (
+	"beli-mang/internal/handler"
+	"beli-mang/internal/repository"
+	"beli-mang/internal/service"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
+	db := s.db.GetDB()
+
 	s.App.Get("/", s.HelloWorldHandler)
 
 	s.App.Get("/health", s.healthHandler)
 
+	userRepo := repository.NewUser()
+
+	userService := service.NewUser(db, userRepo)
+
+	userHandler := handler.NewUser(userService)
+
+	admin := s.App.Group("/admin")
+	admin.Post("/register", userHandler.CreateAdmin)
 }
 
 func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
