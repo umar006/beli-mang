c5d3f6ab7297b45fac6c741eb18fc0caf7f4cc1e
umar006 <umaralfaruq597@gmail.com>
2024-05-25 21:27:13 +0700

    feat: register admin handler

---
+++ internal/handler/user.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"beli-mang/internal/domain"
+	"beli-mang/internal/service"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandler interface {
+	CreateAdmin(ctx *fiber.Ctx) error
+}
+
+type userHandler struct {
+	userService service.UserService
+}
+
+func NewUser(userService service.UserService) UserHandler {
+	return &userHandler{
+		userService: userService,
+	}
+}
+
+func (uh *userHandler) CreateAdmin(ctx *fiber.Ctx) error {
+	var body domain.AdminRequest
+	ctx.BodyParser(&body)
+
+	err := uh.userService.CreateAdmin(ctx.Context(), body)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	return ctx.Status(201).JSON(map[string]string{"message": "success register admin"})
+}
