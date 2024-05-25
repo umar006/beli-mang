94419970bb1f33eb6b3dd9f2b9a6021d03c98038
umar006 <umaralfaruq597@gmail.com>
2024-05-25 16:35:43 +0700

    feat: merchant domain

---
+++ internal/domain/merchant.go
@@ -0,0 +1,42 @@
+package domain
+
+import (
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type MerchantCategoryType int8
+
+const (
+	SmallRestaurant MerchantCategoryType = iota
+	MediumRestaurant
+	LargeRestaurant
+	MerchandiseRestaurant
+	BoothKiosk
+	ConvenienceStore
+)
+
+func (mc MerchantCategoryType) String() string {
+	switch mc {
+	case SmallRestaurant:
+		return "SmallRestaurant"
+	case MediumRestaurant:
+		return "MediumRestaurant"
+	case LargeRestaurant:
+		return "LargeRestaurant"
+	case MerchandiseRestaurant:
+		return "MerchandiseRestaurant"
+	case BoothKiosk:
+		return "BoothKiosk"
+	case ConvenienceStore:
+		return "ConvenienceStore"
+	}
+	return "unknown merchant category"
+}
+
+type Merchant struct {
+	ID        string               `json:"merchantId" db:"id"`
+	CreatedAt int64                `json:"createdAt" db:"created_at"`
+	Category  MerchantCategoryType `json:"merchantCategory" db:"category"`
+	ImageUrl  string               `json:"imageUrl" db:"image_url"`
+	Location  pgtype.Point         `json:"location" db:"location"`
+}
