2408c8f3ed76336138d34d7bbcc539a61b4d5985
umar006 <umaralfaruq597@gmail.com>
2024-05-28 07:19:30 +0700

    feat: merchant id foreign key

--- internal/database/migrations/20240525071700_create_merchant_items_table.up.sql
+++ internal/database/migrations/20240525071700_create_merchant_items_table.up.sql
@@ -5,8 +5,13 @@ CREATE TABLE IF NOT EXISTS merchant_items (
     price numeric NOT NULL,
     category varchar NOT NULL,
     image_url varchar NOT NULL,
+    merchant_id varchar NOT NULL,
 
     CONSTRAINT category_merchant_items_check CHECK (
         category IN ('Beverage', 'Food', 'Snack', 'Condiments', 'Additions')
-    )
+    ),
+    CONSTRAINT merchant_id_merchant_items_merchants_fk 
+        FOREIGN KEY (merchant_id) 
+            REFERENCES merchants (id)
+            ON DELETE CASCADE
 );
