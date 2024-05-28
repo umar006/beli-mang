0443e6093cfc504110da3d68a923b327ca21ac10
umar006 <umaralfaruq597@gmail.com>
2024-05-28 13:43:29 +0700

    feat: get merchant item list repository

--- internal/repository/merchant-item.go
+++ internal/repository/merchant-item.go
@@ -3,12 +3,14 @@ package repository
 import (
 	"beli-mang/internal/domain"
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
 
 type MerchantItemRepo interface {
 	CreateMerchantItem(ctx context.Context, db *pgx.Conn, merchantId string, merchantItem domain.MerchantItem) error
+	GetMerchantItemListByMerchantID(ctx context.Context, db *pgx.Conn, merchantId string) ([]domain.MerchantItemResponse, error)
 }
 
 type merchantItemRepo struct{}
@@ -31,4 +33,30 @@ func (mi *merchantItemRepo) CreateMerchantItem(ctx context.Context, db *pgx.Conn
 	}
 
 	return nil
+}
+
+func (mi *merchantItemRepo) GetMerchantItemListByMerchantID(ctx context.Context, db *pgx.Conn, merchantId string) ([]domain.MerchantItemResponse, error) {
+	fmt.Println(merchantId)
+	query := `SELECT id, created_at, name, category, price, image_url
+	 FROM merchant_items
+	 WHERE merchant_id = $1`
+	rows, err := db.Query(ctx, query, merchantId)
+	if err != nil {
+		return nil, err
+	}
+
+	merchantItemList := []domain.MerchantItemResponse{}
+	for rows.Next() {
+		merchantItem := domain.MerchantItemResponse{}
+		err := rows.Scan(
+			&merchantItem.ID, &merchantItem.CreatedAt, &merchantItem.Name,
+			&merchantItem.Category, &merchantItem.Price, &merchantItem.ImageUrl,
+		)
+		if err != nil {
+			return nil, err
+		}
+		merchantItemList = append(merchantItemList, merchantItem)
+	}
+
+	return merchantItemList, nil
 }
